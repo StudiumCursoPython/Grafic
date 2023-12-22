a42329efa982a028cc7fa3a0c55d5464e76d5f8c
informaticosonline <informaticosonline22@gmail.com>
2023-12-22 11:21:12 +0100

    Curso terminado, comentario de autoría

--- GrafiBasicTempInter.py
+++ GrafiBasicTempInter.py
@@ -1,3 +1,12 @@
+""" 
+Curso Python empresa de 'Lenguaje de Programación Python'
+
+Autor: José Antonio Calvo López
+
+Fecha: Noviembre 2023
+
+"""
+
 # https://aprendeconalf.es/docencia/python/manual/matplotlib/
 
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
